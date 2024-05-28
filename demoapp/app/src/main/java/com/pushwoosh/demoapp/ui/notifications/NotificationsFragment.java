12357de163f3d4b0a592c3834c293cef15c896e3
Andrei Kiselev <akiselev@pushwoosh.com>
2024-05-28 14:27:50 +0200

    init commit

---
+++ demoapp/app/src/main/java/com/pushwoosh/demoapp/ui/notifications/NotificationsFragment.java
@@ -0,0 +1,65 @@
+package com.pushwoosh.demoapp.ui.notifications;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.pushwoosh.demoapp.R;
+import com.pushwoosh.demoapp.databinding.FragmentNotificationsBinding;
+
+public class NotificationsFragment extends Fragment {
+
+    private FragmentNotificationsBinding binding;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        NotificationsViewModel notificationsViewModel =
+                new ViewModelProvider(this).get(NotificationsViewModel.class);
+
+        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        Switch switch1 = binding.switch1;
+        Switch switch2 = binding.switch2;
+
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // Switch is checked
+                    Log.d("", "MESSAGE");
+                } else {
+                    // Switch is unchecked
+                }
+            }
+        });
+
+        switch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // Switch is checked
+                } else {
+                    // Switch is unchecked
+                }
+            }
+        });
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
